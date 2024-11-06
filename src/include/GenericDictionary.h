815745c53632c4cb08d8566474c589f455a3060b
pacmaninbw <paul.chernick@chernicksw.com>
2024-11-06 08:11:09 -0800

    Edits based on latest code review. The getIds() and getNames() functions are no longer virtual. No error reporting to std::cerr, all errors throw exceptions. getIds() and getNames() return std::optional. Testing moved into separate file.

--- src/include/GenericDictionary.h
+++ src/include/GenericDictionary.h
@@ -5,16 +5,12 @@
 #include <concepts>
 #include <exception>
 #include <initializer_list>
-#include <iostream>
+#include <optional>
 #include <ranges>
 #include <stdexcept>
 #include <string>
 #include <vector>
 #include <unordered_map>
-
-#ifdef GD_PERFORMANCE_TEST
-#define GD_UNIT_TEST
-#endif // GD_PERFORMANCE_TEST
 
 /*
  * This class provides a basic conversion of enums or integers to strings. The
@@ -81,13 +77,12 @@ public:
     GenericDictionary(DictID MinValue, DictID MaxValue, std::vector<DictType> definitions);
     virtual ~GenericDictionary() = default;
 
-    virtual DictID getIds(DictName itemName);
-    virtual DictName getNames(DictID id);
+    std::optional<DictID> getIds(DictName itemName);
+    std::optional<DictName> getNames(DictID id);
     bool addAllDefinitions(std::ranges::input_range auto&& definitions);
     bool addAllDefinitions(DictID MinValue, DictID MaxValue, std::ranges::input_range auto&& definitions);
     bool addAllDefinitions(std::initializer_list<DictType> definitions);
     bool addAllDefinitions(DictID MinValue, DictID MaxValue, std::initializer_list<DictType> definitions);
-    void enableExceptions(bool exceptionsEnabled) { throwException = exceptionsEnabled; };
 
 #ifdef GD_UNIT_TEST
     std::vector<DictType> getUserInput() const noexcept { return userInputList; }
@@ -107,10 +102,7 @@ private:
     [[nodiscard]] bool reportDuplicateNameErrors(std::string funcName) noexcept;
     [[nodiscard]] bool hasID(DictID id) noexcept;
     [[nodiscard]] bool hasName(DictName name) noexcept;
-    [[nodiscard]] bool limitsNotSet(std::string funcName, std::string searchValue) noexcept;
-    [[nodiscard]] bool sanityCheck(std::string funcName, std::string searchValue) noexcept;
-    [[nodiscard]] bool searchTableNotInitialized(std::string funcName, std::string searchValue) noexcept;
-    [[nodiscard]] bool sortAndTestIds(std::string& emsg) noexcept;
+    [[nodiscard]] bool sortAndTestIds() noexcept;
     [[nodiscard]] bool testForIDNoneLinearDefinitions(std::string funcName) noexcept;
     [[nodiscard]] bool addDefinition(DictType newDef) noexcept;
 
@@ -124,7 +116,7 @@ private:
     std::unordered_map<DictID, DictName> idSearchTable;
     std::unordered_map<DictName, DictID> nameSearchTable;
     std::vector<DictType> userInputList;
-    bool throwException = false;
+    std::string exceptionWhatMsg;
 };
 
 /******************************************************************************
@@ -142,7 +134,7 @@ MaximumValue{MaxValue}
 
     if (!commonInternalListBuilder("Constructor"))
     {
-        std::logic_error constructorError("GenericDictionary::GenericDictionary(std::initializer_list): Search List Initialization");
+        std::logic_error constructorError(exceptionWhatMsg);
         throw constructorError;
     }
 }
@@ -155,58 +147,33 @@ userInputList{definitions}
 {
     if (!commonInternalListBuilder("Constructor"))
     {
-        std::logic_error constructorError("GenericDictionary::GenericDictionary(std::vector): Search List Initialization Error");
+        std::logic_error constructorError(exceptionWhatMsg);
         throw constructorError;
     }
 }
 
 template <typename DictID, typename DictName>
-DictID GenericDictionary<DictID, DictName>::getIds(DictName itemName)
+std::optional<DictID> GenericDictionary<DictID, DictName>::getIds(DictName itemName)
 {
-    if (sanityCheck("getIds", "name value"))
+    auto definition = nameSearchTable.find(itemName);
+    if (definition != nameSearchTable.end())
     {
-        auto definition = nameSearchTable.find(itemName);
-        if (definition != nameSearchTable.end())
-        {
-            return definition->second;
-        }
-    }
-    else
-    {
-        if (throwException)
-        {
-            std::logic_error constructionError("GenericDictionary::addAllDefinitions(): Errors Occured.");
-            throw constructionError;
-        }
-
+        return definition->second;
     }
 
-    return static_cast<DictID>(MinimumValue);
+    return std::nullopt;
 };
 
 template <typename DictID, typename DictName>
-DictName GenericDictionary<DictID, DictName>::getNames(DictID id)
+std::optional<DictName> GenericDictionary<DictID, DictName>::getNames(DictID id)
 {
-    if (sanityCheck("getNames", "enum value") && (MinimumValue < id && id < MaximumValue))
+    auto definition = idSearchTable.find(id);
+    if (definition != idSearchTable.end())
     {
-        auto definition = idSearchTable.find(id);
-        if (definition != idSearchTable.end())
-        {
-            return definition->second;
-        }
-    }
-    else
-    {
-        if (throwException)
-        {
-            std::logic_error constructionError("GenericDictionary::addAllDefinitions(): Errors Occured.");
-            throw constructionError;
-        }
-
+        return definition->second;
     }
 
-    DictName nameNotFound;
-    return nameNotFound;
+    return std::nullopt;
 }
 
 /*
@@ -228,11 +195,6 @@ bool GenericDictionary<DictID, DictName>::addAllDefinitions(std::ranges::input_r
 {
     bool noErrors = true;
 
-    if (limitsNotSet("addAllDefinitions",""))
-    {
-        return false;
-    }
-
 // There is no guarantee that definitions are a vector so translate
     for (auto const& newDef : definitions)
     {
@@ -242,9 +204,9 @@ bool GenericDictionary<DictID, DictName>::addAllDefinitions(std::ranges::input_r
     if (noErrors)
     {
         noErrors = commonInternalListBuilder("addAllDefinitions()");
-        if (throwException)
+        if (!noErrors)
         {
-            std::logic_error constructionError("GenericDictionary::addAllDefinitions(): Errors Occured.");
+            std::logic_error constructionError(exceptionWhatMsg);
             throw constructionError;
         }
     }
@@ -279,6 +241,7 @@ bool GenericDictionary<DictID, DictName>::addAllDefinitions(DictID MinValue, Dic
 }
 
 #ifdef DEBUG
+#include <iostream>
 template <typename DictID, typename DictName>
 void GenericDictionary<DictID, DictName>::debugDumpList(std::vector<DictType> toDump) noexcept
 {
@@ -313,7 +276,7 @@ void GenericDictionary<DictID, DictName>::debugDumpData() const noexcept
 #endif // DEBUG
 
 /******************************************************************************
- * Protected/Private methods
+ * Private methods
  *****************************************************************************/
 
 template<typename DictID, typename DictName>
@@ -352,56 +315,13 @@ template<typename DictID, typename DictName>
     return true;
 }
 
-template<typename DictID, typename DictName>
-[[nodiscard]] bool GenericDictionary<DictID, DictName>::sanityCheck(std::string funcName, std::string searchValue) noexcept
-{
-    if (limitsNotSet(funcName, searchValue))
-    {
-        return false;
-    }
-// if the limits aren't set there is no need to check searchtable initialization.
-    if (searchTableNotInitialized(funcName, searchValue))
-    {
-        return false;
-    }
-
-    return true;
-}
-
-template<typename DictID, typename DictName>
-[[nodiscard]] bool GenericDictionary<DictID, DictName>::limitsNotSet(std::string funcName, std::string searchValue) noexcept
-{
-    if (!(static_cast<std::size_t>(MaximumValue) > static_cast<std::size_t>(MinimumValue)))
-    {
-        std::string emsg("In GenericDictionary::" + funcName + " (" + searchValue +"): ");
-        emsg += "The minimum and maximum enum values have not been set.";
-        std::cerr << emsg << "\n";
-        return true;
-    }
-
-    return false;
-}
-
-template<typename DictID, typename DictName>
-[[nodiscard]] bool GenericDictionary<DictID, DictName>::searchTableNotInitialized(std::string funcName, std::string searchValue) noexcept
-{
-    if (idSearchTable.size() == 0 || nameSearchTable.size() == 0)
-    {
-        std::string emsg("In GenericDictionary::" + funcName + " (" + searchValue +"): ");
-        emsg += "searchTable has not been initialized.";
-        std::cerr << emsg << "\n";
-        return true;
-    }
-    return false;
-}
-
 /*
  * Sorts the user input. After sorting the input performs various tests
  * on the validity of the input, finding duplicate IDs or missing IDs. The
  * input must be sorted before the the error checking can be performed.
  */
 template <typename DictID, typename DictName>
-[[nodiscard]] bool GenericDictionary<DictID, DictName>::sortAndTestIds(std::string& emsg) noexcept
+[[nodiscard]] bool GenericDictionary<DictID, DictName>::sortAndTestIds() noexcept
 {
     std::sort(userInputList.begin(), userInputList.end(),
         [](DictType defIter1, DictType defIter2) {return  defIter1.id < defIter2.id;});
@@ -417,12 +337,12 @@ template <typename DictID, typename DictName>
         {
             if (id1 == id2)
             {
-                emsg += "duplicate enum values: " + std::to_string(id1) + " Name: " + defIter1->names + " - " + std::to_string(id2) +
+                exceptionWhatMsg += "duplicate enum values: " + std::to_string(id1) + " Name: " + defIter1->names + " - " + std::to_string(id2) +
                     " Name: " + defIter2->names + "\n\t";
             }
             else
             {
-                emsg += "missing enum value: " + std::to_string((id2 - 1)) + "\n\t";
+                exceptionWhatMsg += "missing enum value: " + std::to_string((id2 - 1)) + "\n\t";
             }
             noErrors = false;
         }
@@ -438,9 +358,8 @@ template <typename DictID, typename DictName>
     expectedSize -= 1;
     if (userInputList.size() != expectedSize)
     {
-        std::string emsg("In GenericDictionary::" + funcName + " missing definition values");
-        emsg += " expected size " + std::to_string(expectedSize) + " actual size " + std::to_string(userInputList.size());
-        std::cerr << emsg << "\n";
+        exceptionWhatMsg += "In GenericDictionary::" + funcName + " missing definition values";
+        exceptionWhatMsg += " expected size " + std::to_string(expectedSize) + " actual size " + std::to_string(userInputList.size());
         return true;
     }
 
@@ -481,14 +400,12 @@ template <typename DictID, typename DictName>
         return false;
     }
 
-    std::string emsg("In GenericDictionary::" + funcName + ": Duplicate names found: \n\t");
+    exceptionWhatMsg += "In GenericDictionary::" + funcName + ": Duplicate names found: \n\t";
     for (auto duplicate: duplicates)
     {
-        emsg += "Name: " + duplicate.names + " Integer Value of ID: " +
+        exceptionWhatMsg += "Name: " + duplicate.names + " Integer Value of ID: " +
             std::to_string(static_cast<std::size_t>(duplicate.id)) + "\n\t";
     }
-
-    std::cerr << emsg << "\n";
 
     return true;
 }
@@ -502,14 +419,9 @@ template <typename DictID, typename DictName>
     ) noexcept
 {
     bool noErrors = true; 
-    std::string emsg("In GenericDictionary::" + funcName + ": \n\t");
+    exceptionWhatMsg += "In GenericDictionary::" + funcName + ": \n\t";
 
-    noErrors = sortAndTestIds(emsg);
-    if (!noErrors)
-    {
-        std::cerr << emsg << "\n";
-    }
-
+    noErrors = sortAndTestIds();
 
     return noErrors;
 }
@@ -526,227 +438,6 @@ template <typename DictID, typename DictName>
 
     return false;
 }
-
-/******************************************************************************
- * Unit testing and performance testing of the GenericDictinary class are
- * here to ease the maintenance of the testing and of the class. By default
- * if we are perfomance testing we are also unit testing.
- ******************************************************************************/
-
-#ifdef GD_PERFORMANCE_TEST
-#include "UtilityTimer.h"
-#endif // GD_PERFORMANCE_TEST
-
-#ifdef GD_UNIT_TEST
-#include <iostream>
-
-template <typename DictID, typename DictName>
-bool GenericDictionary<DictID, DictName>::selfUnitTest()
-{
-
-    return true;
-}
-
-template <typename enumDictType>
-static bool testIdToName(
-	GenericDictionary<enumDictType, std::string>& underTest,
-	enumDictType testID,
-	std::string expectedOutput,
-	std::string errorMessage
-)
-{
-    std::string testGetName = underTest.getNames(testID);
-    if (testGetName.compare(expectedOutput) != 0)
-    {
-        std::cerr
-            << "\t" << errorMessage << " Input Value " <<
-            static_cast<std::size_t>(testID)
-            << " Expected Output " << expectedOutput << " " 
-            << " Actual Ouput " << testGetName << "\n";
-            ;
-        return false;
-    }
-
-    return true;
-}
-
-template <typename enumDictType>
-static bool testNameToID(
-	GenericDictionary<enumDictType, std::string>& underTest,
-	std::string testName,
-	enumDictType expectedOutput,
-	std::string errorMessage
-	)
-{
-    enumDictType testGetID = underTest.getIds(testName);
-    if (testGetID != expectedOutput)
-    {
-        std::cerr <<
-            "\t " << errorMessage << " get ID from Name FAILED: "
-            << "Input Name: " << testName
-            << " Expected Output: " << static_cast<std::size_t>(expectedOutput)
-            << " Actual Output: " << static_cast<std::size_t>(testGetID)
-            << "\n";
-        return false;
-    }
-
-    return true;
-}
-
-template <typename  enumDictType> using TestPairs = GenricDictionaryDataPair<enumDictType, std::string>;
-
-template <typename enumDictType>
-static bool testIDtoNameLoop(
-    GenericDictionary <enumDictType, std::string>& underTest,
-    std::vector<GenricDictionaryDataPair<enumDictType, std::string>>& TestData,
-    std::string errorMessage
-)
-{
-    bool allTestsPassed = true;
-
-    for (auto test: TestData)
-    {
-        enumDictType testId = test.id;
-        std::string expectedOutPut = test.names;
-        std::string testGetName = underTest.getNames(testId);
-        if (testGetName.compare(expectedOutPut) != 0)
-        {
-            std::cerr
-                << "\t" << errorMessage << " Input Value " <<
-                static_cast<std::size_t>(testId)
-                << " Expected Output " << expectedOutPut << " " 
-                << " Actual Ouput " << testGetName << "\n";
-                ;
-            allTestsPassed = false;
-            break;
-        }
-    }
-
-    return allTestsPassed;
-}
-
-template <typename enumDictType>
-static bool testNametoIDLoop(
-    GenericDictionary <enumDictType, std::string>& underTest,
-    std::vector<GenricDictionaryDataPair<enumDictType, std::string>>& TestData,
-    std::string errorMessage
-)
-{
-    bool allTestsPassed = true;
-
-    for (auto test: TestData)
-    {
-        std::string testName = test.names;
-        enumDictType expectedOutput = test.id;
-        enumDictType testGetID = underTest.getIds(testName);
-        if (testGetID != expectedOutput)
-        {
-            std::cerr <<
-                "\t " << errorMessage << " get ID from Name FAILED: "
-                << "Input Name: " << testName
-                << " Expected Output: " << static_cast<std::size_t>(expectedOutput)
-                << " Actual Output: " << static_cast<std::size_t>(testGetID)
-                << "\n";
-            allTestsPassed = false;
-            break;
-        }
-    }
-
-    return allTestsPassed;
-}
-
-#ifdef GD_PERFORMANCE_TEST
-/*
- * This function tests the performance of the searches for selected, the first
- * the last and 3 middle values.
- */
-template <typename enumDictType>
-static bool testMinMaxMiddleValues(
-    GenericDictionary <enumDictType, std::string>& underTest,
-    std::vector<TestPairs<enumDictType>>& testData,
-    std::string testName
-)
-{
-    bool allTestsPassed = true;
-    UtilityTimer testTimer;
-
-    std::vector<std::size_t> indexes;
-    indexes.push_back(static_cast<std::size_t>(testData.size() - 1));
-    if (testData.size() > 10)
-    {
-        indexes.push_back(static_cast<std::size_t>((testData.size() * 0.75) - 1));
-        indexes.push_back(static_cast<std::size_t>((testData.size() * 0.5) - 1));
-        indexes.push_back(static_cast<std::size_t>((testData.size() * 0.25) -1));
-    }
-    indexes.push_back(0);
-
-    for (auto tIndex: indexes)
-    {
-        auto tDp = testData[tIndex];
-        std::string testReport = "Performance Test find string from [" + std::to_string(tIndex) + "] ID " + testName + " ";
-        testTimer.startTimer();
-        allTestsPassed = testIdToName<enumDictType>(underTest, tDp.id, tDp.names, testName + " testIDToName()");
-        testTimer.stopTimerAndReport(testReport);
-        if (!allTestsPassed)
-        {
-            return false;
-        }
-    }
-
-    for (auto tIndex: indexes)
-    {
-        auto tDp = testData[tIndex];
-        std::string testReport = "Performance Test find ID from [" + std::to_string(tIndex) + "] Name " + testName + " ";
-        testTimer.startTimer();
-        allTestsPassed = testNameToID<enumDictType>(underTest, tDp.names, tDp.id, testName + " testNameToID()");
-        testTimer.stopTimerAndReport(testReport);
-
-        if (!allTestsPassed)
-        {
-            return false;
-        }
-    }
-
-    return allTestsPassed;
-}
-
-template <typename enumDictType>
-static bool performanceExecution(
-    GenericDictionary <enumDictType, std::string>& underTest,
-    std::vector<TestPairs<enumDictType>>& testData,
-    std::string testName
-)
-{
-    bool allTestsPassed = true;
-    UtilityTimer testTimer;
-
-    testMinMaxMiddleValues<enumDictType>(underTest, testData, testName);
-    if (!allTestsPassed)
-    {
-        return false;
-    }
-
-    testTimer.startTimer();
-    testIDtoNameLoop<enumDictType>(underTest, testData, testName + " testIDtoNameLoop()");
-    testTimer.stopTimerAndReport("Performance Loop Test find string from ID " + testName + " ");
-    if (!allTestsPassed)
-    {
-        return false;
-    }
-
-    testTimer.startTimer();
-    testNametoIDLoop<enumDictType>(underTest, testData, testName + " testNametoIDLoop()");
-    testTimer.stopTimerAndReport("Performance Loop Test find ID from Name " + testName + " ");
-    if (!allTestsPassed)
-    {
-        return false;
-    }
-
-    return allTestsPassed;
-}
-#endif
-
-#endif // GD_UNIT_TEST
 
 #endif // GENERICDICTIONARY_H_
 
